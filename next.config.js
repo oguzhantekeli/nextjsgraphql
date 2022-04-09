cfeaf5d066b66914cd505b80f2dd7765c7af71de
oguzhan <oguzhantekeli@gmail.com>
2022-04-09 19:00:09 +0300

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
+const images = { domains: ["rickandmortyapi.com"] };
 
-module.exports = nextConfig
+module.exports = { nextConfig, images };
